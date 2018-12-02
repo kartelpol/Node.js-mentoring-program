ee0d5afb87a452fcd6b7f6c7d87d66609cac8b68
Palina_Kartsel <Palina_Kartsel@epam.com>
2018-12-02 17:02:37 +0300

    first task configure webpack and babel set up webpack + nodemon bundling (via onBuildEnd hook) add taskConfig create 2 models (User, Product) and config for convinient import all of them

---
+++ app.js
@@ -0,0 +1,7 @@
+import * as config from './config/taskInfo.json';
+import { Product, User } from 'models';
+
+console.log(config.name);
+
+const product = new Product();
+const user = new User();
---
+++ config/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const path = require('path');
+
+const ENTRY_PATH = path.join(__dirname, '../app.js');
+const BUILD_PATH = path.join(__dirname, '../build');
+const PROJECT_ROOT = path.join(__dirname, '../');
+
+const SRC_PATH = path.resolve(__dirname, '../src');
+const MODELS_PATH = path.resolve(__dirname, '../src/models');
+
+const clean = new CleanWebpackPlugin(['build'], {
+    root: PROJECT_ROOT,
+});
+
+module.exports = {
+    entry: ENTRY_PATH,
+    output: {
+      path: BUILD_PATH,
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.json'],
+      alias: {
+        src: SRC_PATH,
+        models: MODELS_PATH,
+      }
+    },
+    watch: this.mode === 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+
+    plugins: [
+        clean,
+        new WebpackShellPlugin({
+          onBuildEnd:  this.mode === 'development' && ['nodemon build/bundle.js --watch build']
+      })
+    ],
+  };
+  
